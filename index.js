c20834d1447704bdfa273a8179ad1e17dcfe745c
Marijn Besseling <njirambem@gmail.com>
2019-09-11 23:06:15 +0200

    its js not c# dummy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http')
 
 http.createServer(function (request, response) {
-	response.WriteHead(200, {"Content-Type": "text/plain"})
+	response.writeHead(200, {"Content-Type": "text/plain"})
 	response.end("Hello, World!\n")
 }).listen(process.env.PORT)
